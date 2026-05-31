0b23ea8dd7ad80e781477dd56c8f201d79f03533
nyas1 <nyas1@users.noreply.github.com>
2026-05-31 14:04:21 +0530

    feat(ui): animate filter expansion and search box entrance

--- docs/.vitepress/theme/components/MobileSpeedDialFab.vue
+++ docs/.vitepress/theme/components/MobileSpeedDialFab.vue
@@ -41,85 +41,87 @@ onUnmounted(() => document.removeEventListener('pointerdown', onGlobalPointerDow
       <i class="material-icons-outlined" aria-hidden="true">tune</i>
       <span>Filters</span>
     </button>
-    <div
-      v-show="filtersExpanded"
-      class="myal-speed-dial-chips"
-      role="group"
-      aria-label="App list filters"
-      @click.stop
-    >
-      <div class="myal-speed-dial-row">
-        <button
-          type="button"
-          :class="['chip', 'no-wave', 'myal-speed-dial-chip--wide', { round: fossOnly }]"
-          :aria-pressed="fossOnly"
-          @click.stop="fossOnly = !fossOnly"
-        >
-          <i class="material-icons-outlined" aria-hidden="true">{{
-            fossOnly ? 'done' : 'close'
-          }}</i>
-          <span>FOSS Only</span>
-        </button>
+    <Transition name="myal-filter-panel">
+      <div
+        v-if="filtersExpanded"
+        class="myal-speed-dial-chips"
+        role="group"
+        aria-label="App list filters"
+        @click.stop
+      >
+        <div class="myal-speed-dial-row">
+          <button
+            type="button"
+            :class="['chip', 'no-wave', 'myal-speed-dial-chip--wide', { round: fossOnly }]"
+            :aria-pressed="fossOnly"
+            @click.stop="fossOnly = !fossOnly"
+          >
+            <i class="material-icons-outlined" aria-hidden="true">{{
+              fossOnly ? 'done' : 'close'
+            }}</i>
+            <span>FOSS Only</span>
+          </button>
+        </div>
+        <div class="myal-speed-dial-row">
+          <button
+            type="button"
+            :class="['chip', 'no-wave', 'myal-speed-dial-chip--wide', { round: hideArchived }]"
+            :aria-pressed="hideArchived"
+            @click.stop="hideArchived = !hideArchived"
+          >
+            <i class="material-icons-outlined" aria-hidden="true">{{
+              hideArchived ? 'done' : 'close'
+            }}</i>
+            <span>Hide archived</span>
+          </button>
+        </div>
+        <div class="myal-speed-dial-row myal-speed-dial-row--quad">
+          <button
+            type="button"
+            :class="['chip', 'no-wave', 'myal-speed-dial-chip--pill', { round: showMD3E }]"
+            :aria-pressed="showMD3E"
+            @click.stop="showMD3E = !showMD3E"
+          >
+            <i class="material-icons-outlined" aria-hidden="true">{{
+              showMD3E ? 'done' : 'close'
+            }}</i>
+            <span>MD3E</span>
+          </button>
+          <button
+            type="button"
+            :class="['chip', 'no-wave', 'myal-speed-dial-chip--pill', { round: showMDY }]"
+            :aria-pressed="showMDY"
+            @click.stop="showMDY = !showMDY"
+          >
+            <i class="material-icons-outlined" aria-hidden="true">{{
+              showMDY ? 'done' : 'close'
+            }}</i>
+            <span>MDY</span>
+          </button>
+          <button
+            type="button"
+            :class="['chip', 'no-wave', 'myal-speed-dial-chip--pill', { round: showMD }]"
+            :aria-pressed="showMD"
+            @click.stop="showMD = !showMD"
+          >
+            <i class="material-icons-outlined" aria-hidden="true">{{
+              showMD ? 'done' : 'close'
+            }}</i>
+            <span>MD</span>
+          </button>
+          <button
+            type="button"
+            :class="['chip', 'no-wave', 'myal-speed-dial-chip--pill', { round: showMY }]"
+            :aria-pressed="showMY"
+            @click.stop="showMY = !showMY"
+          >
+            <i class="material-icons-outlined" aria-hidden="true">{{
+              showMY ? 'done' : 'close'
+            }}</i>
+            <span>MY</span>
+          </button>
+        </div>
       </div>
-      <div class="myal-speed-dial-row">
-        <button
-          type="button"
-          :class="['chip', 'no-wave', 'myal-speed-dial-chip--wide', { round: hideArchived }]"
-          :aria-pressed="hideArchived"
-          @click.stop="hideArchived = !hideArchived"
-        >
-          <i class="material-icons-outlined" aria-hidden="true">{{
-            hideArchived ? 'done' : 'close'
-          }}</i>
-          <span>Hide archived</span>
-        </button>
-      </div>
-      <div class="myal-speed-dial-row myal-speed-dial-row--quad">
-        <button
-          type="button"
-          :class="['chip', 'no-wave', 'myal-speed-dial-chip--pill', { round: showMD3E }]"
-          :aria-pressed="showMD3E"
-          @click.stop="showMD3E = !showMD3E"
-        >
-          <i class="material-icons-outlined" aria-hidden="true">{{
-            showMD3E ? 'done' : 'close'
-          }}</i>
-          <span>MD3E</span>
-        </button>
-        <button
-          type="button"
-          :class="['chip', 'no-wave', 'myal-speed-dial-chip--pill', { round: showMDY }]"
-          :aria-pressed="showMDY"
-          @click.stop="showMDY = !showMDY"
-        >
-          <i class="material-icons-outlined" aria-hidden="true">{{
-            showMDY ? 'done' : 'close'
-          }}</i>
-          <span>MDY</span>
-        </button>
-        <button
-          type="button"
-          :class="['chip', 'no-wave', 'myal-speed-dial-chip--pill', { round: showMD }]"
-          :aria-pressed="showMD"
-          @click.stop="showMD = !showMD"
-        >
-          <i class="material-icons-outlined" aria-hidden="true">{{
-            showMD ? 'done' : 'close'
-          }}</i>
-          <span>MD</span>
-        </button>
-        <button
-          type="button"
-          :class="['chip', 'no-wave', 'myal-speed-dial-chip--pill', { round: showMY }]"
-          :aria-pressed="showMY"
-          @click.stop="showMY = !showMY"
-        >
-          <i class="material-icons-outlined" aria-hidden="true">{{
-            showMY ? 'done' : 'close'
-          }}</i>
-          <span>MY</span>
-        </button>
-      </div>
-    </div>
+    </Transition>
   </div>
 </template>
